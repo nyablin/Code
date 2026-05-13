a7142db895f4b5f35725c74fa39aea50424e2fa8
Wang Lin <87704637+Nemox-renegade-immortal@users.noreply.github.com>
2026-05-13 13:06:44 +0300

    Build OpenGLWorld into bin

--- build_sln.bat
+++ build_sln.bat
@@ -1,10 +1,21 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-cmake -S . -B build -G "Visual Studio 17 2022" -A x64
-if errorlevel 1 cmake -S . -B build -G "Visual Studio 16 2019" -A x64
+set BUILD_DIR=build_vs
+cmake -S . -B "%BUILD_DIR%"
 if errorlevel 1 exit /b 1
-cmake --build build --config Release
+> "%BUILD_DIR%\OpenGLWorld.slnx" (
+echo ^<Solution^>
+echo   ^<Configurations^>
+echo     ^<Platform Name="x64" /^>
+echo   ^</Configurations^>
+echo   ^<Project Path="OpenGLWorld.vcxproj" Id="845e92b7-48be-3561-a2fb-9ab224e71e5a" /^>
+echo   ^<Project Path="ZERO_CHECK.vcxproj" Id="6968d9c7-701f-3309-9fd8-27554f037a75" /^>
+echo   ^<Project Path="_deps/glfw-build/src/glfw.vcxproj" Id="c1160f31-9bfb-3508-b7f8-6bee1d151e27" /^>
+echo ^</Solution^>
+)
+cmake --build "%BUILD_DIR%" --config Release
 if errorlevel 1 exit /b 1
-echo Solution: build\OpenGLWorld.sln
-echo Executable: build\Release\OpenGLWorld.exe
+if exist "%BUILD_DIR%\OpenGLWorld.sln" echo Solution: %BUILD_DIR%\OpenGLWorld.sln
+if exist "%BUILD_DIR%\OpenGLWorld.slnx" echo Solution: %BUILD_DIR%\OpenGLWorld.slnx
+echo Executable: bin\OpenGLWorld.exe
